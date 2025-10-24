41a42d9faad264d4d0ed443ed97d611b82180aee
Alessandro Serpi <alessandro.serpi@me.com>
2025-10-24 19:40:11 +0200

    build: improve gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,34 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
-var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var esbuild = require('gulp-esbuild');
 
-// compile scss to css
+// Compile SCSS to CSS
 gulp.task('sass', function () {
     return gulp.src('./sass/styles.scss')
-        .pipe(sass({style: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({basename: 'styles.min'}))
-        .pipe(gulp.dest('./css'));
+        .pipe(sass({ includePaths: ['node_modules'], style: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ basename: 'styles.min' }))
+        .pipe(gulp.dest('./dist'));
 });
 
-// watch changes in scss files and run sass task
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-// minify js
-gulp.task('minify-js', function () {
+// Bundle and minify JS (Bootstrap + your code)
+gulp.task('bundle-js', function () {
     return gulp.src('./js/scripts.js')
-        .pipe(uglify())
-        .pipe(rename({basename: 'scripts.min'}))
-        .pipe(gulp.dest('./js'));
+        .pipe(esbuild({
+            bundle: true,
+            minify: true,
+            sourcemap: true,
+            outfile: 'scripts.min.js',
+            target: 'es2017',
+        }))
+        .pipe(gulp.dest('./dist'));
 });
 
-// copy decrypt worker
+// Optional: copy decrypt worker
 gulp.task('copy-decrypt-worker', function () {
     return gulp.src('./js/decrypt.js')
         .pipe(gulp.dest('./js'));
 });
 
-// default task
-gulp.task('default', gulp.series('sass', 'minify-js', 'copy-decrypt-worker'));
+// Default task
+gulp.task('default', gulp.series('sass', 'bundle-js', 'copy-decrypt-worker'));
